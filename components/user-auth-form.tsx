6a4cc9844f5164a757123098a6803a11a58bc177
Matt <mr.matthewc.king@gmail.com>
2023-05-15 13:23:48 -0600

    work
    -better style matching for sign in
    -remove postmark (email should be handled by supabase)

--- components/user-auth-form.tsx
+++ components/user-auth-form.tsx
@@ -16,19 +16,21 @@ export default function UserAuthForm() {
         magicLink={true}
         appearance={{
           className: {
-            anchor: "",
+            anchor:
+              "!text-sm !text-muted-foreground hover:!text-brand !underline !underline-offset-4",
             button:
-              "!bg-primary !text-primary-foreground hover:!bg-primary/90 !rounded-md !h-10 sm:!h-9 sm:!px-3 lg:!h-11 lg:!px-8",
-            container: " ",
-            divider: "",
+              "!bg-primary !text-sm !text-primary-foreground hover:!bg-primary/90 !rounded-md !h-10 sm:!h-9 sm:!px-3 lg:!h-11 lg:!px-8",
+            container: "",
+            divider: "!bg-input",
             input:
               "!h-10 !rounded-md border !border-input !bg-transparent !px-3 !py-2 !text-sm !ring-offset-background file:!border-0 file:!bg-transparent file:!text-sm file:!font-medium placeholder:!text-muted-foreground focus-visible:!outline-none focus-visible:!ring-2 focus-visible:!ring-ring focus-visible:!ring-offset-2 disabled:!cursor-not-allowed disabled:!opacity-50",
             label: "!mb-2 !text-xs hover:!cursor-pointer",
-            loader: "",
-            message: "",
+            loader: "w-4 h-4 mr-2 animate-spin",
+            message: "!text-xs !text-red-600",
           },
         }}
       />
     </div>
   )
 }
+
--- env.mjs
+++ env.mjs
@@ -7,10 +7,6 @@ export const env = createEnv({
     // See https://next-auth.js.org/deployment.
     NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(1),
     NEXT_PUBLIC_SUPABASE_URL: z.string().url().min(1),
-    SMTP_FROM: z.string().min(1),
-    POSTMARK_API_TOKEN: z.string().min(1),
-    POSTMARK_SIGN_IN_TEMPLATE: z.string().min(1),
-    POSTMARK_ACTIVATION_TEMPLATE: z.string().min(1),
     STRIPE_API_KEY: z.string().min(1),
     STRIPE_WEBHOOK_SECRET: z.string().min(1),
     STRIPE_PRO_MONTHLY_PLAN_ID: z.string().min(1),
@@ -22,10 +18,6 @@ export const env = createEnv({
   runtimeEnv: {
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    SMTP_FROM: process.env.SMTP_FROM,
-    POSTMARK_API_TOKEN: process.env.POSTMARK_API_TOKEN,
-    POSTMARK_SIGN_IN_TEMPLATE: process.env.POSTMARK_SIGN_IN_TEMPLATE,
-    POSTMARK_ACTIVATION_TEMPLATE: process.env.POSTMARK_ACTIVATION_TEMPLATE,
     STRIPE_API_KEY: process.env.STRIPE_API_KEY,
     STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
     STRIPE_PRO_MONTHLY_PLAN_ID: process.env.STRIPE_PRO_MONTHLY_PLAN_ID,
