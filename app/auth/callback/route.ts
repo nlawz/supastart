933917b4a6e7f814abd94e68f76b05daaa95b2af
n <nlaw.dev@gmail.com>
2024-01-03 16:45:52 -0800

    slowly

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,16 +1,19 @@
-import { cookies } from "next/headers"
-import { NextResponse } from "next/server"
-import type { NextRequest } from "next/server"
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 
-import type { Database } from "@/types/db"
+
+
+import type { Database } from "@/types/db";
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get("code")
 
   if (code) {
-    const supabase = createRouteHandlerClient<Database>({ cookies })
+    const cookieStore = cookies();
+    const supabase = createRouteHandlerClient<Database>({cookies: () => cookieStore})
     await supabase.auth.exchangeCodeForSession(code)
   }
 
